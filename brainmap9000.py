4f2216729d4aeab1bf5a95a76bfa61f95fbea08e
irahamm <148943839+irahamm@users.noreply.github.com>
2024-11-20 08:45:14 +0200

    added license notice in the beginning of the code file

--- brainmap9000.py
+++ brainmap9000.py
@@ -8,6 +8,19 @@ import networkx as nx
 import pandas as pd
 import math
 import xml.etree.ElementTree as ET
+# First - License notice:
+'''
+'brainmap9000' is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+'''
+# Now that we got that covered, let's dive in
 """
 PART A - LOAD THE DATA
 """
